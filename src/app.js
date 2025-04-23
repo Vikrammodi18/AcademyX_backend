85bbfc2e2e7ff033a89d337174c94a9fff4cf0b7
Vikrammodi18 <suchit.sbd.9631@gmail.com>
2025-04-24 00:26:40 +0530

    fix: restrict CORS origin to localhost for improved security

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const app = express()
 const cors = require("cors")
 const cookieParser = require("cookie-parser")
 app.use(cors({
-    origin : "*",
+    origin : "http://localhost:5173",
     credentials: true 
 }))
 app.use(express.json({limit:"16kb"}))
