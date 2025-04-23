f101f9c90d63a6874f51419f79c606c7da5973b8
Vikrammodi18 <suchit.sbd.9631@gmail.com>
2025-04-24 00:14:48 +0530

    fix: correct formatting in CORS middleware configuration

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const cors = require("cors")
 const cookieParser = require("cookie-parser")
 app.use(cors({
     origin : "*",
-  
+    credentials: true 
 }))
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
