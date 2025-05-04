e1d9d505358bbb9a18bc050e2f6b9affef9242e7
Vikrammodi18 <suchit.sbd.9631@gmail.com>
2025-05-04 21:30:29 +0530

    fix: correct typo in courseId parameter for toggleLike route

--- src/routes/like.route.js
+++ src/routes/like.route.js
@@ -3,7 +3,7 @@ const router = Router()
 const { toggleLike } = require("../controllers/like.controller.js")
 const {verifyJWT} = require("../middleware/auth.middleware.js")
 
-router.route("/:couseId/toggleLike").get(verifyJWT,toggleLike)
+router.route("/:courseId/toggleLike").get(verifyJWT,toggleLike)
 
 
 module.exports = router
